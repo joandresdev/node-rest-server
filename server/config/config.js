e8043aa7feb1bb7e96103b4abee5c2fac113a61d
Jose Andres Romero Ramirez <joandres098@gmail.com>
2019-11-03 04:53:39 -0500

    Variable entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDb;
 if (process.env.NODE_ENV === 'dev') {
     urlDb = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDb = 'mongodb+srv://tacotest:Q62IppAhrjDhYODq@cluster0-agjbp.mongodb.net/cafe';
+    urlDb = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDb;
--- server/server.js
+++ server/server.js
@@ -35,3 +35,4 @@ app.listen(process.env.PORT, () => {
 // Q62IppAhrjDhYODq
 // tacotest
 // mongodb+srv://tacotest:Q62IppAhrjDhYODq@cluster0-agjbp.mongodb.net/cafe
+// mongorestore --host Cluster0-shard-0/cluster0-shard-00-00-agjbp.mongodb.net:27017,cluster0-shard-00-01-agjbp.mongodb.net:27017,cluster0-shard-00-02-agjbp.mongodb.net:27017 --ssl --username tacotest --password <PASSWORD> --authenticationDatabase admin
